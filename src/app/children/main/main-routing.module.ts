03e0c8f023871d93ca07e2211c9659ff7a2f3a05
nikitandro <nik.vish.93@mail.ru>
2023-06-05 12:29:49 +0500

    removed scrollbar and added mobile version of burger button menu

--- src/app/children/main/main-routing.module.ts
+++ src/app/children/main/main-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { EmployeeListPageComponent } from './pages/employee-list-page/employee-list-page.component';
-import { authGuard } from '../auth/guards/auth.guard';
 import {
     EmployeePersonalDataPageWebComponent
 } from './pages/employee-personal-data-page/employee-personal-data.page.web.component';
